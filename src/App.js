91113ef50da63001a80b61fa4c6cbdbf3f853dbf
saragilani <s.g.ahmed163@gmail.com>
2023-02-10 14:10:34 +0000

    state in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+const  SERVER_URL = "http://localhost:8080/"
+
 function App() {
+  
+  const [users, setUsers] = useState([]);
+  const [carees, setCarees] = useState([]); 
+  const [needs, setNeeds] = useState([]); 
+  const [error, setError] = useState("");
+  
+  useEffect(() => {
+    fetch(`${SERVER_URL}users/`)
+    .then((response) => response.json())
+    .then((data) => setUsers(data))
+    .catch((error) => setError(error.message))
+  },  []);
+  
+  useEffect(() => {
+    fetch(`${SERVER_URL}carees`)
+    .then((response) => response.json())
+    .then((data) => setCarees(data))
+    .catch((error) => setError(error.message))
+  },  []);
+  
+  useEffect(() => {
+    fetch(`${SERVER_URL}needs`)
+    .then((response) => response.json())
+    .then((data) => setNeeds(data))
+    .catch((error) => setError(error.message))
+  },  []);
+  
+  console.log(carees);
+
   return (
-    <></>
+    <>
+  
+    </>
   );
 }
 
