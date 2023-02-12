fd4ce40fe1fb3f3ed8106a164e3340674fce529f
Berna Yasar <bernayasar@hotmail.co.uk>
2023-02-12 15:58:17 +0000

    a few additions that broke the app somehow(the commented out stuff)

--- src/components/Caree/Caree.js
+++ src/components/Caree/Caree.js
@@ -1,9 +1,28 @@
-const Caree = ({caree}) => {
-
+const Caree = ({caree }) => {
+    // toDoListCaree, setToDoListCaree
     // const listItem = caree.toDoList;
     // const toDos = listItem.map((toDo) => {
     //     return <Caree key={toDo.id} toDo={toDo} />
     // })
+
+    // making a method that will show all the current todos and then add the new todos
+
+    // const addNewToDoCaree = (newToDo) => {
+
+    //     fetch(`${SERVER_URL}needs`,{
+    //         method: "POST",
+    //         headers: {"Content-Type": "application/json"},
+    //         body: JSON.stringify(newToDo),
+    //     })
+    //     .then((response) =>response.json())
+    //     .then((response)=> {
+    //     setToDoListCaree([...toDoListCaree,response]);
+    //     })
+    // }
+
+    // I think this might need to be in a form... 
+    // we need to make useStates for each state of the todo and then assign it via e.target.value
+
 
     return (
         <>
--- src/components/Caree/CareeList.js
+++ src/components/Caree/CareeList.js
@@ -1,3 +1,4 @@
+// import { useState } from "react";
 import Caree from "./Caree";
 
 const CareeList = ({carees}) => {
@@ -6,12 +7,15 @@ const CareeList = ({carees}) => {
         return <Caree key={caree.id} caree={caree}/>
     })
 
+    // making a useState of the to do list of carees, to be able to pass it to other places as props
+    //    const[toDoListCaree, setToDoListCaree] = useState(carees.toDoList)
+    // going to pass down the toDoListCaree and setDoListCaree to Caree.js as props 
 
 
     return (  
         <>
         {careeComponents}
-
+        {/* <Caree toDoListCaree={toDoListCaree} setToDoListCaree={setToDoListCaree}/> */}
         </>
     );
 }
