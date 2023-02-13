2cbafec1b158e951b235230b2909070e199f3660
Emma Bayley-Melendez <e.melendez98@outlook.com>
2023-02-13 15:00:01 +0000

    added UserContext to App.js

--- src/App.js
+++ src/App.js
@@ -4,23 +4,67 @@ import NeedContainer from './containers/NeedContainer';
 import UserContainer from './containers/UserContainer';
 import CareeContainer from './containers/CareeContainer';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { createContext, useContext, useState } from 'react';
+
 
 // login form + user State
  // useContext = /needs/{need.id}/{user.id}
  // ^ to get logged in user
  // logged in user is set at highest level, then drilled down
 
+const UserContext = createContext (null); 
+
 function App() {
+
+const [user, setUser] = useState()   
   
   
   return (
     <>
+    <UserContext.Provider value={user}>
+    <Form />
+    </UserContext.Provider>
     <p>App.js</p>
     {/* <UserContainer />
     <NeedContainer /> */}
-    <CareeContainer />
+    {/* <CareeContainer /> */}
     </>
   );
 }
+
+function Form() {
+  return (
+  <Panel title = "Welcome to Orbit">
+    <Button>Sign Up</Button> 
+    <Button>Log-in</Button> 
+  </Panel>
+  )
+}
+
+
+function Panel({title, children}){
+  const user = useContext(UserContext); 
+  const className = 'panel-' + user; 
+
+  return (
+    <section className={className}>
+    <h1>{title}</h1>
+    {children}
+    </section>
+  )
+}
+
+
+function Button({children}) {
+  const user = useContext(UserContext); 
+  const className = 'button-' + user; 
+
+  return (
+    <button className={className}>
+      {children}
+    </button>
+  )
+}
+
 
 export default App;
