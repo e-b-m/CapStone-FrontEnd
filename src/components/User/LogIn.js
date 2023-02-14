b609f69b02a0b9ba886f6e2872260a5cf6fe1478
saragilani <s.g.ahmed163@gmail.com>
2023-02-14 17:02:49 +0000

    login submit

--- src/components/User/LogIn.js
+++ src/components/User/LogIn.js
@@ -6,7 +6,7 @@ const LogIn = ({users}) => {
 
     const [emailAddress, setEmailAddress] = useState("")
     const [password, setPassword] = useState("")
-    const [loggedInUser, setLoggedInUser] = useState({})
+    const [user, setUser] = useState()
 
     const userLogin = ((userInfo) => {
         fetch(`${SERVER_URL}users/login`, {
@@ -17,7 +17,7 @@ const LogIn = ({users}) => {
         
         .then((response) => response.json())
         .then((response) => {
-            setLoggedInUser({response})
+            setUser(response)
         })
     
     })
@@ -29,8 +29,8 @@ const LogIn = ({users}) => {
             password
         }
             userLogin(logInBody)
-            setEmailAddress("");
-            setPassword("");
+            // setEmailAddress("");
+            // setPassword("");
         }
 
     return (  
@@ -41,21 +41,20 @@ const LogIn = ({users}) => {
             id="emailAddress"
             type="text"
             placeholder="Email Address" 
-            value= {emailAddress}
-            onChange= {(event)=>setEmailAddress(event.target.value)}
+            defaultValue= {emailAddress}
+            // onChange= {(event)=>setEmailAddress(event.target.value)}
             />
 
             <input 
             id="password"
             type="password"
             placeholder="Password" 
-            value= {password}
-            onChange= {(event)=>setPassword(event.target.value)}
+            defaultValue= {password}
+            // onChange= {(event)=>setPassword(event.target.value)}
             />
 
             <input type="submit" value="submit"/>
 
-            {console.log("logged in")}
         </form>
 
         </>
