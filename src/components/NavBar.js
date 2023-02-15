bfae530759be3662d9e0cfd848575a76fb904e60
Caitlin Millar <caitlin-millar@outlook.com>
2023-02-15 13:06:48 +0000

    able to show profile links only when logged in

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,14 @@ import { UserContext, UserContextProvider } from './User/ReferenceDataContext';
 
 const NavBar = () => {
     const [loggedInUser, setLoggedInUser] = useContext(UserContext)
-    
+
+    // const logged = (loggedInUser) => {
+    //     return (
+    //     loggedInUser ? null 
+    //     : <><LogIn setLoggedInUser={setLoggedInUser}/>
+    //     <button className="userprofile-btn"><Link to="/UserProfile"> User Profile </Link></button>
+    //     </>)
+    // } 
     
 
     return ( 
@@ -13,11 +20,11 @@ const NavBar = () => {
         <p> Logged in user nav bar </p>
         <img/>
 
-        {/* <button className="signup-btn"> <Link to="/SignUp"> Sign up </Link> </button> */}
+        {loggedInUser ? null : <button className="signup-btn"> <Link to="/SignUp"> Sign up </Link> </button>}
         {loggedInUser ? null : <LogIn setLoggedInUser={setLoggedInUser}/>}
-        <button className="userprofile-btn"><Link to="/UserProfile"> User Profile </Link></button>
-        <button className="careeprofile-btn"><Link to="/CareeProfile"> Caree Profile </Link></button>
-        <button className="bubblepage-btn"><Link to="/BubblePage"> Bubble Page </Link></button>
+        {loggedInUser ? <button className="userprofile-btn"><Link to="/UserProfile"> User Profile </Link></button> : null}
+        {loggedInUser ? <button className="careeprofile-btn"><Link to="/CareeProfile"> Caree Profile </Link></button> : null}
+        {loggedInUser ? <button className="bubblepage-btn"><Link to="/BubblePage"> Bubble Page </Link></button> : null}
         
         </> 
     );
