bceeb3548c04a8d7d4a79ea188fc1997e095ba3a
Caitlin Millar <caitlin-millar@outlook.com>
2023-02-15 19:56:03 +0000

    unable to fix no map method error

--- src/components/Caree/CareeProfile.js
+++ src/components/Caree/CareeProfile.js
@@ -5,29 +5,30 @@ import Caree from "./Caree"
 import CareeList from "./CareeList"
 import { useParams } from "react-router-dom"
 import CareeContainer from "../../containers/CareeContainer"
+import User from "../User/User"
 
 const  SERVER_URL = "http://localhost:8080/"
 
 const CareeProfile = () => {
-    const [setLoggedInUser] = useContext(UserContext);
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
     const [carees, setCarees] = useState([]); 
     let {id} = useParams();
 
-    // const userComps = loggedInUser.map((you) => {
-    //     return <loggedInUser key={you}/>
+    // const userComps = loggedInUser.map((caree) => {
+    //     return <loggedInUser key={id} caree={caree}/>
     // })
 
     // get caree by id 
-    const myCaree= ((carees) => {
-        console.log(carees);
-        fetch(`${SERVER_URL}carees/1`,{
-            method: "GET",
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(carees)
-        })
-        .then((response) => response.json())
-        .then((response) => setLoggedInUser(response))
-      })
+    // const myCaree= ((carees) => {
+    //     console.log(carees);
+    //     fetch(`${SERVER_URL}carees/1`,{
+    //         method: "GET",
+    //         headers: {'Content-Type': 'application/json'},
+    //         body: JSON.stringify(carees)
+    //     })
+    //     .then((response) => response.json())
+    //     .then((response) => setLoggedInUser(response))
+    //   })
 
     return(
         <>
@@ -35,7 +36,7 @@ const CareeProfile = () => {
         <p>dskjfhalskdf</p>
 
         <setLoggedInUser setLoggedInUser= {setLoggedInUser} carees={carees}/>
-       
+        
   
         </>
     )
