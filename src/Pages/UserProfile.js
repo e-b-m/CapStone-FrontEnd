d0c76fbcbec7325035cd22f6637ee706571b8d09
Berna Yasar <bernayasar@hotmail.co.uk>
2023-02-15 21:32:06 +0000

    trying to get the todo list of user

--- src/Pages/UserProfile.js
+++ src/Pages/UserProfile.js
@@ -1,10 +1,28 @@
+import { UserContext} from '../components/User/ReferenceDataContext';
+import { useContext } from "react";
 
+const UserProfile = ({toDoList}) => {
+    
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext)
 
-const UserProfile = () => {
+    console.log(loggedInUser);
+    // const userToDoList = loggedInUser.map((userToDoList) => {
+    //     return userToDoList.toDoList.map((eachToDo) =>{
+    //         return console.log(eachToDo);
+    //     })
+    // })
+    console.log(UserContext);
+   
 
+    const userToDoList = Object.entries(loggedInUser).map((toDoList) => {
+        return toDoList;
+    })
     return ( 
         <>
-            <p>User Profile</p>
+            <h1 className="welcome-user">Welcome {loggedInUser.name}</h1>
+
+            <h2> Here are your list of to-dos:</h2>
+            <ul> {userToDoList} </ul>
         </> 
     );
 }
