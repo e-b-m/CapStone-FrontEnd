27cc9d061a931099a2578e0c02974dcbcc744a5b
saragilani <s.g.ahmed163@gmail.com>
2023-02-16 12:13:33 +0000

    committing with main

--- src/components/Pages/UserProfile.js
+++ src/components/Pages/UserProfile.js
@@ -1,11 +1,6 @@
 import { UserContext} from '../User/ReferenceDataContext';
 import { useContext } from "react";
-<<<<<<< HEAD:src/components/Pages/UserProfile.js
-import "../stylesheets/UserProfile.css";
-
-=======
-import Need from '../components/Need/Need';
->>>>>>> main:src/Pages/UserProfile.js
+import Need from '../Need/Need';
 
 const UserProfile = () => {
     
@@ -16,23 +11,12 @@ const UserProfile = () => {
     })
 
     return ( 
-<<<<<<< HEAD:src/components/Pages/UserProfile.js
-        <div className='UserProfileContainer'>
-            {/* <h1 className="welcome-user">Welcome {loggedInUser.name}</h1> */}
-            <h1 className='UserHeading'>Welcome {loggedInUser.name}!</h1>
-            {/* <p>{loggedInUser.userToDoList2}</p> */}
-
-            <h2 className='toDosHeading'> Here are your list of to-dos:</h2>
-            {/* <ul> {toDoList} </ul> */}
-        </div> 
-=======
         <>
             <h1>Welcome {loggedInUser.name}!</h1>
 
             <h2> Here are your list of to-dos:</h2>
             {userToDoList}
         </> 
->>>>>>> main:src/Pages/UserProfile.js
     );
 }
  
