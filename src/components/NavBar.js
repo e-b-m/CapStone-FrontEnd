7c0e2225fe288c9e2fc77aec885477e505a4270e
saragilani <s.g.ahmed163@gmail.com>
2023-02-16 11:25:49 +0000

    single navbar changing

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -15,61 +15,102 @@ const NavBar = () => {
 
     return ( 
         <>
-    <MDBNavbar bgColor='info' className='text-center text-lg-start text-muted'>
-        <p className='p-4'>  
-                <a href='' className='text-reset fw-bold mb-4'>ORBIT</a>   
-        </p>
+    {/* <MDBNavbar bgColor='info' className='text-center text-lg-start text-muted'> */}
         <img/>
 
         {loggedInUser ? 
         
             <>
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-                <button className="navbtn"><Link to="/UserProfile"> User Profile </Link></button>
-        </li>
-        <li class="nav-item">
-                <button className="navbtn"><Link to="/CareeProfile"> Caree Profile </Link></button> 
-        </li>
-        <li class="nav-item">
-                <button className="navbtn"><Link to="/BubblePage"> Bubble Page </Link></button> 
-        </li>
-      </ul>
+              <nav class="navbar navbar-expand-lg navbar-light bg-info">
+                <div class="container-fluid">
+                  <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-mdb-toggle="collapse"
+                    data-mdb-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                  >
+                    <i class="fas fa-bars"></i>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    {/* <!-- Navbar brand --> */}
+                    <a class="navbar-brand mt-2 mt-lg-1" href="#">
+                      <img
+                        src={logo}
+                        height="75"
+                        alt="ORBIT Logo"
+                        loading="lazy"
+                      />
+                    </a>
+                    {/* <!-- Left links --> */}
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                                      <a className="nav-link"><Link to="/UserProfile"> User Profile </Link></a>
+                              </li>
+                      <li class="nav-item">
+                                      <a className="nav-link"><Link to="/CareeProfile"> Caree Profile </Link></a> 
+                              </li>
+                      <li class="nav-item">
+                                      <a className="nav-link" class="nav-link"><Link to="/BubblePage"> Bubble Page </Link></a> 
+                              </li>
+                    </ul>
+                    {/* <!-- Left links --> */}
+                  </div>
 
-                <button className='navbtn' onClick={signOut}>Sign Out</button>
+                  <div class="d-flex align-items-center">
+                    <a class="text-reset me-3" href="#">
+                      <i class="fas fa-shopping-cart"></i>
+                    </a>
+                    <div class="dropdown">
+                      <a
+                        class="text-reset me-3 dropdown-toggle hidden-arrow"
+                        href="#"
+                        id="navbarDropdownMenuLink"
+                        role="button"
+                        data-mdb-toggle="dropdown"
+                        aria-expanded="false"
+                      >
+                        <i class="fas fa-bell"></i>
+                        <span class="badge rounded-pill badge-notification bg-danger">10</span>
+                      </a>
+                    </div>
+                    {/* USER LOGO */}
+                    <div class="dropdown">
+                      <a
+                        class="dropdown-toggle d-flex align-items-center hidden-arrow"
+                        href="#"
+                        id="navbarDropdownMenuAvatar"
+                        role="button"
+                        data-mdb-toggle="dropdown"
+                        aria-expanded="false"
+                      >
+                        <img
+                          src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
+                          class="rounded-circle"
+                          height="70"
+                          alt="Black and White Portrait of a Man"
+                          loading="lazy"
+                        />
+                      </a>
+                              <button className='navbtn' onClick={signOut}>Sign Out</button>
+                    </div>
+                  </div>
+                </div>
+              </nav>
+
+        
             
             </> 
-        
         : 
         
             <>
             
-                <MDBContainer className='text-center text-md-start mt-5'>
-                <button className="signup-btn"> <Link to="/SignUp"> Sign up </Link> </button>
-                <button className="login-btn"><Link to="/LogIn"> Login </Link></button>
-    </MDBContainer>
-                
-            </>
-        }
-    </MDBNavbar>
-
-    {/* <!-- Navbar --> */}
+                    {/* <!-- Navbar --> */}
 <nav class="navbar navbar-expand-lg navbar-light bg-info">
   {/* <!-- Container wrapper --> */}
   <div class="container-fluid">
-    {/* <!-- Toggle button --> */}
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-mdb-toggle="collapse"
-      data-mdb-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <i class="fas fa-bars"></i>
-    </button>
-
     {/* <!-- Collapsible wrapper --> */}
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       {/* <!-- Navbar brand --> */}
@@ -81,40 +122,18 @@ const NavBar = () => {
           loading="lazy"
         />
       </a>
-      {/* <!-- Left links --> */}
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link" href="#">User Profile</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Caree Profile</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Bubble Page</a>
-        </li>
-      </ul>
-      {/* <!-- Left links --> */}
     </div>
-
     <div class="d-flex align-items-center">
-      <a class="text-reset me-3" href="#">
-        <i class="fas fa-shopping-cart"></i>
-      </a>
-      <div class="dropdown">
-        <a
-          class="text-reset me-3 dropdown-toggle hidden-arrow"
-          href="#"
-          id="navbarDropdownMenuLink"
-          role="button"
-          data-mdb-toggle="dropdown"
-          aria-expanded="false"
-        >
-          <i class="fas fa-bell"></i>
-          <span class="badge rounded-pill badge-notification bg-danger">10</span>
-        </a>
-      </div>
+      
       {/* USER LOGO */}
       <div class="dropdown">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <li class="nav-item">
+      <button className="login-btn"><Link to="/LogIn"> Login </Link></button>                </li>
+        <li class="nav-item">
+           <button className="nav-item"> <Link to="/SignUp"> Sign up </Link> </button>           
+        </li>
+      </ul>
         <a
           class="dropdown-toggle d-flex align-items-center hidden-arrow"
           href="#"
@@ -123,18 +142,16 @@ const NavBar = () => {
           data-mdb-toggle="dropdown"
           aria-expanded="false"
         >
-          <img
-            src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
-            class="rounded-circle"
-            height="70"
-            alt="Black and White Portrait of a Man"
-            loading="lazy"
-          />
         </a>
       </div>
     </div>
   </div>
 </nav>
+
+            </>
+        }
+    {/* </MDBNavbar> */}
+
         </> 
 
     );
