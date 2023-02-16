e9202f42dbb6742d64fd9974f2e681ceb4ea263c
Berna Yasar <bernayasar@hotmail.co.uk>
2023-02-16 10:31:05 +0000

    got the to do list to render

--- src/Pages/UserProfile.js
+++ src/Pages/UserProfile.js
@@ -1,36 +1,21 @@
 import { UserContext} from '../components/User/ReferenceDataContext';
 import { useContext } from "react";
+import Need from '../components/Need/Need';
 
 const UserProfile = () => {
     
     const [loggedInUser, setLoggedInUser] = useContext(UserContext)
 
-    console.log(loggedInUser);
-    // const userToDoList = loggedInUser.map((userToDoList) => {
-    //     return userToDoList.toDoList.map((eachToDo) =>{
-    //         return console.log(eachToDo);
-    //     })
-    // })
-    // console.log(UserContext);
-   
-
-    const userToDoList = Object.values(loggedInUser).map((toDoList) => {
-        return console.log(toDoList);
+    const userToDoList = loggedInUser.toDoList.map((toDo) => {
+        return <Need need={toDo} key={toDo.id}/>
     })
 
-    const userToDoList2 = Object.values(userToDoList).map((toDoList) => {
-        return console.log(toDoList);
-    })
-
-    
     return ( 
         <>
-            {/* <h1 className="welcome-user">Welcome {loggedInUser.name}</h1> */}
             <h1>Welcome {loggedInUser.name}!</h1>
-            {/* <p>{loggedInUser.userToDoList2}</p> */}
 
             <h2> Here are your list of to-dos:</h2>
-            {/* <ul> {toDoList} </ul> */}
+            {userToDoList}
         </> 
     );
 }
